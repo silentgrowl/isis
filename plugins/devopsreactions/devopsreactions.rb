e4c57d328cd06602e3356e0855be1af4cb90c891
Chris Odlaug <codlaug@wetpaint-inc.com>
2013-10-02 11:26:18 -0700

    Devops reactions

---
+++ plugins/devopsreactions/devopsreactions.rb
@@ -0,0 +1,25 @@
+require 'nokogiri'
+require 'open-uri'
+
+class AnimalsBeingJerks < Isis::Plugin::Base
+
+  TRIGGERS = %w(!devops !devopsreactions)
+
+  def respond_to_msg?(msg, speaker)
+    TRIGGERS.include? msg.downcase
+  end
+
+  private
+
+  def response_html
+    page = Nokogiri.HTML(open('http://devopsreactions.tumblr.com/random'))
+    img = page.css('.item_content p img').attr('src').value
+    "<img src=\"#{img}\">"
+  end
+
+  def response_text
+    page = Nokogiri.HTML(open('http://devopsreactions.tumblr.com/random'))
+    title = page.css('.post_title a').text
+    "#{title}"
+  end
+end
